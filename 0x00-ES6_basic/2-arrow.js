3d1c51edd4c89684431dfe7b1ff9979d47299b60
codenvibes <terrymk1999@gmail.com>
2024-05-31 16:44:07 +0300

    Refactor: Convert addNeighborhood method to ES6 arrow function

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -3,11 +3,11 @@
 * TASK: Rewrite the standard function to use ES6’s arrow syntax of the function add (it will be an anonymous function after)
 */
 
-export default function getNeighborhoodsList() {
-  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+export default function getNeighborhoodsList () {
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square']
 
   this.addNeighborhood = (newNeighborhood) => {
-    this.sanFranciscoNeighborhoods.push(newNeighborhood);
-    return this.sanFranciscoNeighborhoods;
-  };
+    this.sanFranciscoNeighborhoods.push(newNeighborhood)
+    return this.sanFranciscoNeighborhoods
+  }
 }
