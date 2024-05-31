3ed51fe551e8a7c4e144d3b4fad5011190b4c42a
codenvibes <terrymk1999@gmail.com>
2024-05-31 15:22:32 +0300

    Add Task 2 code pre-mod

---
+++ 0x00-ES6_basic/2-arrow.js
@@ -0,0 +1,14 @@
+/*
+* AUTH: bugsnvibes
+* TASK: Rewrite the standard function to use ES6’s arrow syntax of the function add (it will be an anonymous function after)
+*/
+
+export default function getNeighborhoodsList() {
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+
+  const self = this;
+  this.addNeighborhood = function add(newNeighborhood) {
+    self.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return self.sanFranciscoNeighborhoods;
+  };
+}
