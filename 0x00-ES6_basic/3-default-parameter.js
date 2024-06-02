9e065cf1774492a17f2b860f67bfcbf34bc3b970
codenvibes <terrymk1999@gmail.com>
2024-06-02 09:23:19 +0300

    Refactor: Simplify getSumOfHoods function by using default parameter values

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -1,3 +1,9 @@
+/*
+* AUTH: bugsnvibes
+* TASK: Condense the internals of the function to 1 line - without
+        changing the name of each function/variable.
+*/
+
 export default function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 19) {
   return initialNumber + expansion1989 + expansion2019;
 }
