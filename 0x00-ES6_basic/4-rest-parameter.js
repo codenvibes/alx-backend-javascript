38b5137a658bcf22402f419c9bd5b0d364ef8566
codenvibes <terrymk1999@gmail.com>
2024-06-02 09:36:46 +0300

    Feat: Add rest parameter to return the count of arguments passed to the function

--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -4,6 +4,6 @@
         to it using the rest parameter syntax
 */
 
-export default function returnHowManyArguments() {
-
+export default function returnHowManyArguments(...args) {
+  return args.length;
 }
