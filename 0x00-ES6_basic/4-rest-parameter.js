67f8edb95f36350f529ae0e4fc6d11d066d8e14c
codenvibes <terrymk1999@gmail.com>
2024-06-02 09:32:36 +0300

    Add task 4 code pre-mod

---
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -0,0 +1,9 @@
+/*
+* AUTH: bugsnvibes
+* TASK: Modify the function to return the number of arguments passed
+        to it using the rest parameter syntax
+*/
+
+export default function returnHowManyArguments() {
+
+}
