1e19f9978fa99fbe5ea9ea1dcf9195cbeaa2a0af
codenvibes <terrymk1999@gmail.com>
2024-06-03 17:08:27 +0300

    Add Task 7 code pre-mod

---
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -0,0 +1,15 @@
+/**
+ * AUTH: bugsnvibes
+ * TASK: Modify the following function’s budget object to simply use
+        the keyname instead.
+*/
+
+export default function getBudgetObject(income, gdp, capita) {
+  const budget = {
+    income: income,
+    gdp: gdp,
+    capita: capita,
+  };
+
+  return budget;
+}
