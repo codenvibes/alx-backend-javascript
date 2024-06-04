039a19b5cf5f36a20d035c55f4d156b5827a4947
codenvibes <terrymk1999@gmail.com>
2024-06-04 15:07:59 +0300

    Add Task 10 code pre-mod

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,15 @@
+/**
+* AUTH: bugsnvibes
+* TASK: Rewrite the function appendToEachArrayValue to use ES6’s
+        for...of operator. And don’t forget that var is not 
+        ES6-friendly.
+*/
+
+export default function appendToEachArrayValue(array, appendString) {
+  for (var idx in array) {
+    var value = array[idx];
+    array[idx] = appendString + value;
+  }
+
+  return array;
+}
