a2235b9452516d66e66f66587320f7735a7716bf
codenvibes <terrymk1999@gmail.com>
2024-06-04 15:48:14 +0300

    Add Task 11 code pre-mod

---
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -0,0 +1,11 @@
+/**
+* AUTH: bugsnvibes
+* TASK: Write a function named createEmployeesObject that will receive
+        two arguments:
+        - `departmentName` (String)
+        - `employees` (Array of Strings)
+*/
+
+export default function createEmployeesObject(departmentName, employees) {
+
+}
