d19d7170c701bf556992e96f3faa21781ffc5ccf
codenvibes <terrymk1999@gmail.com>
2024-06-05 17:03:36 +0300

    Feat: Add getFullResponseFromAPI function with promise implementation

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -2,18 +2,18 @@
  * AUTH: bugsnvibes
  * A Function that returns a promise based on a boolean parameter.
  *
- * @param {boolean} success - The boolean parameter that determines if the promise resolves or rejects.
- * @returns {Promise<object>} A promise that resolves with an object containing `status` and `body` attributes if `success` is true,
- *                            or rejects with an error object if `success` is false. 
+ * @param {boolean} success - The boolean parameter that determines if the promise resolves or
+   rejects.
+ * @returns {Promise<object>} A promise that resolves with an object containing `status` and `body`
+   attributes if `success` is true, or rejects with an error object if `success` is false.
 */
 
 export default function getFullResponseFromAPI(success) {
-  const arr = new Promise((resolve, reject) => {
-      if (success) {
-          resolve({ status: 200, body: 'Success' });
-      } else {
-          reject(new Error('The fake API is not working currently'));
-      }
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve({ status: 200, body: 'Success' });
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
   });
-  return arr;
 }
