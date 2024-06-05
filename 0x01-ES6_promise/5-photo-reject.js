53b3845b3d95859d999c3dfc019f57859b0004a2
codenvibes <terrymk1999@gmail.com>
2024-06-05 19:22:32 +0300

    Feat: Implement uploadPhoto function to reject with an error

---
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -0,0 +1,13 @@
+/**
+ * AUTH: bugsnvibes
+ * Uploads a photo with the given file name.
+ * 
+ * @param {string} fileName - The name of the file to upload.
+ * @returns {Promise<Error>} A Promise that rejects with an Error indicating that the file cannot be processed.
+ */
+
+export default function uploadPhoto(fileName) {
+  return new Promise((resolve, reject) => {
+    reject(new Error(`${fileName} cannot be processed`));
+  });
+}
