c06e1b2a0a94676f89dde1469ab2d3ac959a395b
codenvibes <terrymk1999@gmail.com>
2024-06-05 20:24:30 +0300

    Feat: Implement divideFunction with error handling for division by zero

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,7 +1,7 @@
 /**
  * AUTH: bugsnvibes
  * Divides the given numerator by the denominator.
- * 
+ *
  * @param {number} numerator - The number to be divided.
  * @param {number} denominator - The number by which to divide.
  * @returns {number} The result of the division.
@@ -10,7 +10,7 @@
 
 export default function divideFunction(numerator, denominator) {
   if (denominator === 0) {
-    throw Error('cannot divide by 0');
+    throw new Error('cannot divide by 0');
   }
   return numerator / denominator;
 }
