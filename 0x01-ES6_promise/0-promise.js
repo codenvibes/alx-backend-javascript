6fa4b7a1aef8e400b0af99ea947ea4494b222deb
codenvibes <terrymk1999@gmail.com>
2024-06-05 16:26:32 +0300

    Feat: Implement getResponseFromAPI to return a Promise

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -2,6 +2,8 @@
  * AUTH: bugnvibes
  * TASK: Return a Promise using this prototype function getResponseFromAPI()
 */
+
 export default function getResponseFromAPI() {
+  // eslint-disable-next-line no-unused-vars
   return new Promise((resolve, reject) => {});
 }
