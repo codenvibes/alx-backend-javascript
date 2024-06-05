d4ade67937097622b859612a97ac57cbec6c348d
codenvibes <terrymk1999@gmail.com>
2024-06-05 13:12:36 +0300

    Feat: Implement getResponseFromAPI to return a Promise

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,12 @@
+/**
+ * AUTH: bugnvibes
+ * TASK: Return a Promise using this prototype function getResponseFromAPI()
+*/
+
+export default function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Success");
+    }, 1000);
+  });
+}
