d944ebca08137bc3342ca54b083f8ae9ebd9d810
codenvibes <terrymk1999@gmail.com>
2024-06-05 16:34:27 +0300

    Initial commit

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,3 +1,12 @@
+/**
+* AUTH: bugsnvibes
+* TASK: Write a function named `createReportObject` whose parameter,
+        `employeesList`, is the return value of the previous function
+        `createEmployeesObject`.
+        `createReportObject` should return an object containing the
+        key `allEmployees` and a method property called `getNumberOfDepartments`.
+*/
+
 export default function createReportObject(employeesList) {
   return {
       allEmployees: { ...employeesList },
