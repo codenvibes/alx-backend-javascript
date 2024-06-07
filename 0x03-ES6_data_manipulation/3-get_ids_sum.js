353b6ab663e32a4ff66ce3fd1024e6bc28f1d891
codenvibes <terrymk1999@gmail.com>
2024-06-07 14:56:46 +0300

    Feat: Create getStudentIdsSum function using reduce

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,8 +1,9 @@
 /**
  * AUTH: bugsnvibes
  * Takes an object representing students and calculates the sum of their IDs.
- *
- * @param {Object} studentObj - An object where the keys are student identifiers and the values are objects containing student details, including an 'id' property.
+
+ * @param {Object} studentObj - An object where the keys are student identifiers and the
+   values are objects containing student details, including an 'id' property.
  * @returns {number} - The sum of all student IDs.
  */
 
