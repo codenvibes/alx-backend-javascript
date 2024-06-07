7973a5ea0ae9ae14a25322a90869096cfef344f5
codenvibes <terrymk1999@gmail.com>
2024-06-07 13:50:29 +0300

    Feat: Replace occurrences of 1 with 100

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,16 @@
+/**
+ * Updates unique items in a Map by replacing their values with 100 if they are currently set to 1.
+ * @param {Map} map - The Map object to be updated.
+ * @throws {Error} Throws an error if the input is not a Map.
+ */
+
+export default function updateUniqueItems(map) {
+  if (!(map instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+  map.forEach((value, key) => {
+    if (value === 1) {
+      map.set(key, 100);
+    }
+  });
+}
