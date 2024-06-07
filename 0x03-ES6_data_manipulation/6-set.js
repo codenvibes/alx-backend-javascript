b7e17ce1c32aef99c9bd78f159aeea8e65a7fdfd
codenvibes <terrymk1999@gmail.com>
2024-06-07 12:22:26 +0300

    Feat: Add setFromArray function to create Set from array

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,10 @@
+/**
+ * AUTH: bugsnvibes
+ * Creates a Set from an array.
+ * @param {Array} arr - The array from which to create the Set.
+ * @returns {Set} - A Set containing the elements of the input array.
+ */
+
+export default function setFromArray(arr) {
+  return new Set(arr);
+}
