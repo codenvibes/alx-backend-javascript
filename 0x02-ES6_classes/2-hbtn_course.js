6d720b50a34b2d60f09e38af406f0ef56124473e
codenvibes <terrymk1999@gmail.com>
2024-08-05 19:27:55 +0300

    Feat: Add HolbertonCourse class with getters and setters for name, length, and students

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,48 +1,22 @@
-/**
- * Represents a Holberton course.
- */
 export default class HolbertonCourse {
-  /**
-   * Creates a new HolbertonCourse.
-   * @param {string} name - The name of the course.
-   * @param {number} length - The length of the course.
-   * @param {string[]} students - The students enrolled in the course.
-   */
   constructor(name, length, students) {
     this._name = name;
     this._length = length;
     this._students = students;
   }
 
-  /**
-   * Gets the name of the course.
-   * @return {string} The name of the course.
-   */
   get name() {
     return this._name;
   }
 
-  /**
-   * Gets the length of the course.
-   * @return {number} The length of the course.
-   */
   get length() {
     return this._length;
   }
 
-  /**
-   * Gets the students enrolled in the course.
-   * @return {string[]} The students enrolled in the course.
-   */
   get students() {
     return this._students;
   }
 
-  /**
-   * Sets the name of the course.
-   * @param {string} courseName - The new name of the course.
-   * @throws {TypeError} If the name is not a string.
-   */
   set name(courseName) {
     if (typeof courseName === 'string') {
       this._name = courseName;
@@ -51,11 +25,6 @@ export default class HolbertonCourse {
     }
   }
 
-  /**
-   * Sets the length of the course.
-   * @param {number} courseLength - The new length of the course.
-   * @throws {TypeError} If the length is not a number.
-   */
   set length(courseLength) {
     if (typeof courseLength === 'number') {
       this._length = courseLength;
@@ -64,11 +33,6 @@ export default class HolbertonCourse {
     }
   }
 
-  /**
-   * Sets the students enrolled in the course.
-   * @param {string[]} value - The new list of students.
-   * @throws {TypeError} If the students are not an array of strings.
-   */
   set students(value) {
     if (!(value instanceof Array)) {
       throw new TypeError('Students must be an array of strings');
