d7d58e8e5f8c25f4f6a3aeb1db8a98b7a0ecd3b7
codenvibes <terrymk1999@gmail.com>
2024-08-05 19:02:39 +0300

    Feat: Implement Airport class with getters and setters for name and code

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,26 +1,55 @@
+/**
+ * Represents an airport.
+ */
 export default class Airport {
+  /**
+   * Creates an instance of the Airport class.
+   * @param {string} name - The name of the airport.
+   * @param {string} code - The airport code.
+   * @throws {Error} If name or code is not a string.
+   */
   constructor(name, code) {
-    if (typeof name !== 'string' || typeof code !== 'string') throw new Error();
+    if (typeof name !== 'string' || typeof code !== 'string') throw new Error('Name and code must be strings');
     this._name = name;
     this._code = code;
   }
 
+  /**
+   * Gets the name of the airport.
+   * @return {string} The name of the airport.
+   */
   get name() {
     return this._name;
   }
 
+  /**
+   * Sets the name of the airport.
+   * @param {string} value - The new name of the airport.
+   */
   set name(value) {
     this._name = value;
   }
 
+  /**
+   * Gets the code of the airport.
+   * @return {string} The code of the airport.
+   */
   get code() {
     return this._code;
   }
 
+  /**
+   * Sets the code of the airport.
+   * @param {string} value - The new code of the airport.
+   */
   set code(value) {
     this._code = value;
   }
 
+  /**
+   * Returns the airport code as the string representation of the object.
+   * @return {string} The airport code.
+   */
   get [Symbol.toStringTag]() {
     return this.code;
   }
