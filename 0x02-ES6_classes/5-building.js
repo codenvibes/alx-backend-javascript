9ad05035ba504323f94f547103de7c59fdfcd040
codenvibes <terrymk1999@gmail.com>
2024-08-05 19:43:43 +0300

    Feat: Implement  class with constructor, getter/setter for sqft, and method for evacuation warning

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,44 +1,20 @@
-/**
- * Represents a building.
- */
 export default class Building {
-  /**
-   * Creates an instance of a Building.
-   * @param {number} sqft - The square footage of the building.
-   */
   constructor(sqft) {
+    if (typeof sqft !== 'number') throw new Error();
     this._sqft = sqft;
+    this.evacuationWarningMessage();
   }
 
-  /**
-   * Method to be overridden by classes extending Building.
-   * Throws an error if not overridden.
-   * @throws {Error} If called directly on Building class.
-   */
-  evacuationWarningMessage() {
-    if (this.constructor !== Building) {
-      throw new Error('Class extending Building must override evacuationWarningMessage');
-    }
-  }
-
-  /**
-   * Gets the square footage of the building.
-   * @return {number} The square footage of the building.
-   */
   get sqft() {
     return this._sqft;
   }
 
-  /**
-   * Sets the square footage of the building.
-   * @param {number} sqftSize - The new square footage of the building.
-   * @throws {TypeError} If sqftSize is not a number.
-   */
-  set sqft(sqftSize) {
-    if (typeof sqftSize === 'number') {
-      this._sqft = sqftSize;
-    } else {
-      throw new TypeError('sqft must be a number');
-    }
+  set sqft(value) {
+    this._sqft = value;
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  evacuationWarningMessage() {
+    if (this.constructor.name !== 'Building') throw new Error('Class extending Building must override evacuationWarningMessage');
   }
 }
